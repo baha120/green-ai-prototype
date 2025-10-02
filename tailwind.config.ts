2bc96a8e972462dec2bfa7be40b0c5a7b9344009
Felix Tom Krusche <felix.krusche@piazzablu.com>
2025-10-02 13:34:13 +0200

    init

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // für App-Router
+    "./pages/**/*.{js,ts,jsx,tsx}", // falls du Seiten mit pages nutzt
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // optional, falls du einen src-Ordner nutzt
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
